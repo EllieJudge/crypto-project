4fa41c5f5f34065cb2d1069f32175ce79eeb575e
EllieJudge <judge_ellie@hotmail.com>
2019-08-23 14:29:49 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Button from '@material-ui/core/Button';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import axios from 'axios';
 
-function App() {
-	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
-	)
+class App extends Component {
+	componentDidMount(){
+		axios.get('https://api.coingecko.com/api/v3/coins?order=rank_desc&per_page=100')
+		.then(function (response) {
+			console.log('success')
+			console.log(response);
+		})
+		.catch(function (err) {
+			console.log('err')
+			console.log(err)
+		})
+	}
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+
+				<h2>Git test hellooooo</h2>
+				
+				<Button variant="contained">
+					Default
+				</Button>
+				<SvgIcon>
+				<path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+			</SvgIcon>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
